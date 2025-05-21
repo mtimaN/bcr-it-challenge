8791116119561f3ad08ee58f2885f301e1cd8e71
filipdumitrascu <filip.dumitrascu@gmail.com>
2025-05-22 00:08:13 +0300

    Implemented ads

--- dashboard/src/Components/Discover/AdsManager/AdsManager.jsx
+++ dashboard/src/Components/Discover/AdsManager/AdsManager.jsx
@@ -1,75 +1,73 @@
 import React from 'react';
 import './AdsManager.css';
 
-import smartCard from '../../../assets/ads/'
-
 const adsByCluster = {
   0: [
     {
       title: "Economisește mai ușor",
       description: "Vezi cele mai bune conturi de economii.",
-      image: ""
+      image: "images/ads/economyAccount.jpg"
     },
     {
       title: "Termen lung, câștig mare",
       description: "Depozite avantajoase pentru tine.",
-      image: ""
+      image: "/images/ads/depositFavorable.jpg"
     },
     {
       title: "Analizează-ți cheltuielile",
       description: "Grafice lunare pentru control financiar.",
-      image: ""
+      image: "/images/ads/financialGraphic.jpg"
     }
   ],
   1: [
     {
       title: "Ai cheltuieli mari?",
-      description: "Descoperă carduri de credit smart.",
-      image: ""
+      description: "Descoperă cardurile de credit smart.",
+      image: "images/ads/smartCard.jpg"
     },
     {
       title: "Credit rapid, fără griji",
       description: "Aplică 100% online pentru împrumut.",
-      image: ""
+      image: "images/ads/loan.jpg "
     },
     {
       title: "Folosește la maxim overdraft-ul",
       description: "Vezi limitele disponibile.",
-      image: ""
+      image: "images/ads/overdraft.jpg"
     }
   ],
   2: [
     {
       title: "George te poate ajuta",
       description: "Descoperă beneficiile contului digital.",
-      image: ""
+      image: "images/ads/digitalAccount.jpg"
     },
     {
       title: "Transferuri mai rapide",
       description: "Încearcă plățile instant.",
-      image: ""
+      image: "images/ads/contactless.jpg"
     },
     {
       title: "Economisește fără efort",
       description: "Setează un plan automat.",
-      image: ""
+      image: "images/ads/financePlan.jpg"
     }
   ],
   3: [
     {
       title: "Trimite bani rapid",
       description: "Transfer instant către prieteni.",
-      image: ""
-    },
-    {
-      title: "Cheltuie smart",
-      description: "Vezi unde se duc banii tăi.",
-      image: ""
+      image: "images/ads/fastTransfers.jpg"
     },
     {
       title: "Economii simple",
       description: "Economisește automat când cheltui.",
-      image: ""
+      image: "images/ads/economyAuto.jpg"
+    },
+    {
+      title: "Cheltuie smart",
+      description: "Vezi unde se duc banii tăi.",
+      image: "images/ads/moneyLocation.jpg"
     }
   ]
 };
