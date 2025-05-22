928928d07a22500269b77de8eb28c180b6a9d4fa
Luca Botez <luca.botez01@gmail.com>
2025-05-22 14:03:45 +0300

    home page implementation

--- dashboard/src/Components/Home/Home.jsx
+++ dashboard/src/Components/Home/Home.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import Balance from './Balance'
+import Transactions from './Transactions'
+import SpendingVisual from './SpendingVisual';
 
-const Home = () => {
+import React, { useState } from 'react';
+
+import './Home.css'
+
+const Home = ({ theme, setTheme }) => {
+
+  /* change theme logic */
+  const toggle_mode = () => {
+    theme === 'light' ? setTheme('dark') : setTheme('light');
+  };
+
   return (
-    <div>
-      
+    <div className="home-container">
+      <Balance theme={theme} setTheme={setTheme}/>
+      <Transactions theme={theme} setTheme={setTheme}/>
+      <SpendingVisual theme={theme} setTheme={setTheme}/>
     </div>
   )
 }
